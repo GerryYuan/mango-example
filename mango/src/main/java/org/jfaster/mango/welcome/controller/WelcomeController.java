843881673e6928aa04869649c197957b85053b72
gerry <gerry@yunio.com>
2016-07-27 18:54:21 +0800

    必填

--- mango/src/main/java/org/jfaster/mango/welcome/controller/WelcomeController.java
+++ mango/src/main/java/org/jfaster/mango/welcome/controller/WelcomeController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.gerry.common.framework.helper.ViewModelHelper;
@@ -17,11 +18,10 @@ public class WelcomeController extends BaseController {
 
 	@Autowired
 	private WelcomeService welcomeService;
-	
-	
+
 	@RequestMapping(value = "", method = RequestMethod.GET)
 	@ResponseBody
-	public ViewModelResult<?> welcome(Integer id) {
+	public ViewModelResult<?> welcome(@RequestParam(required = true, value = "id") Integer id) {
 		return ViewModelHelper.OKViewModelResult(welcomeService.getById(id));
 	}
 
